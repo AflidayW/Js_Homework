ef46fcda5b937c1766bea9cc1444a3e0e73da0be
Afliday <aflidaywork@gmail.com>
2026-01-20 17:05:56 +0300

    lesson2_01_fixed

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -19,6 +19,12 @@ if (isAdmin || isVerifiedUser) {
     if (hasSpecialPermission || hasTemporaryPass) {
         isAccess = true
     }
+    else {
+        isAccess = false
+    }
 
+}
+else {
+    isAccess = false
 }
 // your code
