fb81ea54d381b9643ea8633ae48ab4dff15bd584
Afliday <aflidaywork@gmail.com>
2026-01-21 16:34:48 +0300

    l3_01_fixed

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,5 +1,6 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(num1, num2) {
-    return sum = num1 + num2
+    return sum = (num1 + num2) / 2
 }
+
