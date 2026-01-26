419ba2ca6242a98b22356ee34018af961cab861b
Afliday <aflidaywork@gmail.com>
2026-01-26 18:59:59 +0300

    Fidex L06_02

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -25,10 +25,10 @@ console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
 const map = (array, callback) => {
-  const result = []
+  const new_array = []
   for (let index = 0; index < array.length; index++) {
     const element = array[index]
-    result.push(callback(element, index))
+    new_array.push(callback(element, index))
   }
-  return result
+  return new_array
 }
