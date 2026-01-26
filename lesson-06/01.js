5162a6aa201fffcff38285ecb98b51f1dc3ff9ed
Afliday <aflidaywork@gmail.com>
2026-01-26 17:31:09 +0300

    l6_02

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -33,3 +33,10 @@ const filter = (array, callback) => {
   }
   return result
 }
+const numbers = [1, 2, 3, 4, 5]
+
+const oddNumbers = filter(numbers, (element, index) => {
+  return element % 2 !== 0
+});
+
+console.log(oddNumbers) // Должен вывести: [1, 3, 5]
